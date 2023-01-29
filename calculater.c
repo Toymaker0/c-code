96a33e58d81a93e66cd10e65ebbaa85175483642
Sree Vasanthan k <123319138+Toymaker0@users.noreply.github.com>
2023-01-29 21:19:53 +0530

    Add files via upload
    Addition

---
+++ calculater.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+int x,y,z;
+ printf("Enter the number x: " );
+ scanf ("%d",&x);
+ printf("Enter the number y :");
+ scanf("%d",&y);
+ z=x+y;
+ printf("The sum is :%d",z);
+return 0;
+}
